c25a9173efb3088a89281a4c887750a7bd4c58f7
Thomas Guillouët <thomas.guillouet@outlook.com>
2023-06-30 22:06:01 +0200

    Filling the spinboxes

--- src/ui/overlay_infos.rs
+++ src/ui/overlay_infos.rs
@@ -12,6 +12,11 @@ pub struct OverlayDetails {
     active_state_switch: gtk::Switch,
     
     pub name_entry: gtk::Entry,
+    pub x_pos_spin: gtk::SpinButton,
+    pub y_pos_spin: gtk::SpinButton,
+    pub width_spin: gtk::SpinButton,
+    pub height_spin: gtk::SpinButton,
+
     save_button: gtk::Button,
 
     switch_handler_id: Option<SignalHandlerId>,
@@ -33,6 +38,10 @@ impl OverlayDetails {
             active_state_switch: gtk::Switch::default(),
 
             name_entry: gtk::Entry::default(),
+            x_pos_spin: OverlayDetails::create_spinbutton(),
+            y_pos_spin: OverlayDetails::create_spinbutton(),
+            width_spin: OverlayDetails::create_spinbutton(),
+            height_spin: OverlayDetails::create_spinbutton(),
             save_button: gtk::Button::builder().label("Save").build(),
 
             switch_handler_id: None,
@@ -46,6 +55,11 @@ impl OverlayDetails {
         overlay_details.container.add(&form);
 
         overlay_details
+    }
+    
+    fn create_spinbutton() -> gtk::SpinButton {
+        let adjustment = gtk::Adjustment::new(0.0, 0.0, 5000.0, 1.0, 1.0, 0.0);
+        gtk::SpinButton::new(Some(&adjustment), 1.0, 0)
     }
     
     fn create_header(&mut self) -> gtk::Widget {
@@ -69,25 +83,32 @@ impl OverlayDetails {
         let form_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
 
         form_box.add(&self.name_entry);
+        form_box.add(&self.x_pos_spin);
+        form_box.add(&self.y_pos_spin);
+        form_box.add(&self.width_spin);
+        form_box.add(&self.height_spin);
         form_box.add(&self.save_button);
 
         form_box.into()
     }
 
     pub fn set_current_overlay(&mut self, overlay: LayoutConfig) {
-        // Important: Disable the events BEFORE assigning new values :)
-        if let Some(signal_handler) = self.switch_handler_id.take() {
-            self.active_state_switch.disconnect(signal_handler);
-        }
-        if let Some(signal_handler) = self.save_handler_id.take() {
-            self.save_button.disconnect(signal_handler);
-        }
+        self.disconnect_signals();
 
         // Update the form entries
         self.title.set_text(&overlay.name());
-        self.name_entry.set_text(&overlay.name());
         self.active_state_switch.set_state(overlay.is_active());
+
+        self.name_entry.set_text(&overlay.name());
+        self.x_pos_spin.set_value(overlay.x() as f64);
+        self.y_pos_spin.set_value(overlay.y() as f64);
+        self.width_spin.set_value(overlay.width() as f64);
+        self.height_spin.set_value(overlay.height() as f64);
         
+        self.setup_signals(overlay);
+    }
+    
+    fn setup_signals(&mut self, overlay: LayoutConfig) {
         let overlay_cloned = overlay.clone();
         let event_sender = self.event_sender.clone();
         self.switch_handler_id = Some(
@@ -100,7 +121,7 @@ impl OverlayDetails {
                 Inhibit(true)
             })
         );
-
+    
         let overlay_cloned = overlay.clone();
         let event_sender = self.event_sender.clone();
         self.save_handler_id = Some(
@@ -114,6 +135,16 @@ impl OverlayDetails {
                 })
             })
         );
+    }
+
+    fn disconnect_signals(&mut self) {
+        // Important: Disable the events BEFORE assigning new values :)
+        if let Some(signal_handler) = self.switch_handler_id.take() {
+            self.active_state_switch.disconnect(signal_handler);
+        }
+        if let Some(signal_handler) = self.save_handler_id.take() {
+            self.save_button.disconnect(signal_handler);
+        }
     }
 }
 
